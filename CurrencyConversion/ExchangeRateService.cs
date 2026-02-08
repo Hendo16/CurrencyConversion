f98968a4c2b0de22fe1aeeecc509139b3a27a8a6
Nathan Henderson <nathhendo@hotmail.com>
2026-02-08 18:35:12 +1100

    Initial commit

---
+++ CurrencyConversion/ExchangeRateService.cs
@@ -0,0 +1,19 @@
+﻿namespace CurrencyConversion;
+
+record ExchangeRateResponse(string currency, double value)
+{
+    public string Currency { get; set; }
+    public double Value { get; set; }
+}
+
+public class ExchangeRateService
+{
+    private readonly HttpClient _httpClient;
+
+    public ExchangeRate(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<List<
+}
---
+++ CurrencyConversionTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyConversionTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
